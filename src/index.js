c2139e87273780ec32124674fadf290807f59f67
Miranda.Motsinger <z002v4q@dca9047d63d1.hq.target.com>
2017-09-22 09:17:46 -0500

    only committing src because I'm terrified of having to remove stuff later

---
+++ src/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Button from 'react-bootstrap/lib/Button';
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
+
+/*
+ *  An Action is anything that can be done ingame. It renders as a button.
+ *  They need to do something eventually...and they need to be prepopulated
+ *  from some kind of static list.
+ */
+ class Action extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       readyText: props.readyText,
+       waitingText: null   // shows when the button is "on cooldown" (e.g. "Working...")
+     }
+   }
+
+   render() {
+     return (
+       <Button className="action-button" block>{this.state.readyText}</Button>
+     )
+   }
+ }
+
+ /*
+  * A main class, more or less. The goal right now is to break down the horrific
+  * render function into smaller classes.
+  */
+
+class Screen extends React.Component {
+  renderAction(action) {
+    return (
+      <Action readyText={action} />
+    );
+  }
+
+  render() {
+    return (
+      <Grid className="screen">
+        <Row className="game-header">
+          IDLE &#9734; IDOL
+        </Row>
+        <Row className="content">
+          <Col className="sidebar" lg={2} md={2}>
+            <ButtonGroup vertical block>
+              <Button className="menu-button" bsStyle="primary" block>Home</Button>
+              <Button className="menu-button" block>Downtown</Button>
+              <Button className="menu-button" block>Talent Agency</Button>
+              <Button className="menu-button" block>Studio</Button>
+              <Button className="menu-button" block>Manager Profile</Button>
+              <Button className="menu-button" block>Idolpedia</Button>
+            </ButtonGroup>
+          </Col>
+          <Col className="location" lg={10} md={10}>
+            <Row className="location-header">
+              Home
+            </Row>
+            <Row className="location-content">
+              <Col className="actions" lg={5} md={5}>
+                <ButtonGroup vertical block>
+                  {this.renderAction("Work")}
+                  {this.renderAction("Buy clothes online")}
+                  {this.renderAction("Research idols")}
+                </ButtonGroup>
+              </Col>
+              <Col className="log" lg={5} md={5}>
+                You arrived at home.
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Screen />,
+  document.getElementById('root')
+);
